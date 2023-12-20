64a3bb47512869e75c8458bfce5f3a2d76d2981b
Borislav Stoyanov <bostoycontact@gmail.com>
2023-12-20 18:31:26 +0200

    Burn LP tokens

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -5,6 +5,8 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 import {Math} from "./libraries/Math.sol";
 
 error InsufficientLiquidityMinted();
+error TransferFailed();
+error InsufficientLiquidityBurned();
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
@@ -14,6 +16,8 @@ interface IERC20 {
 
 contract UniswapV2Pair is ERC20, Math {
     event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1);
+    event Sync(uint112 reserve0, uint112 reserve1);
 
     uint256 constant MINIMUM_LIQUIDITY = 1000; // 1e15
 
@@ -32,15 +36,26 @@ contract UniswapV2Pair is ERC20, Math {
         (uint112 _reserve0, uint112 _reserve1,) = getReserves();
         uint256 balance0 = IERC20(token0).balanceOf(address(this));
         uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        // Measure the amount of tokens that are sent as part of the transaction
         uint256 amount0 = balance0 - _reserve0;
         uint256 amount1 = balance1 - _reserve1;
 
         uint256 liquidity;
 
+        // If the pool empty, that is, liquidity tokens have a total supply of zero, then no liquidity has been provided yet.
+        //
         if (totalSupply == 0) {
             liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
             _mint(address(0), MINIMUM_LIQUIDITY);
         } else {
+            //The fact that the user will get the worse of the two ratios (amount0 / _reserve0 or amount1 / _reserve1) they provide
+            //incentivizes them to increase the supply of token0 and token1 without changing the ratio of token0 and token1.
+            //If we took the maximum of the two ratios, someone could supply one additional token1 (at a cost of $100) and raise the pool value to $300.
+            //They’ve increase the pool value by 50%. However, under the maximum calculation,
+            //they would get minted 1 LP tokens, meaning they own 50% of the supply of the LP tokens,
+            //since the total circulating supply is now 2 LP tokens.
+            //Now they control 50% of the $300 pool (worth $150) by only depositing $100 of value.
+            //This is clearly stealing from other LP providers.
             liquidity = Math.min((amount0 * totalSupply) / _reserve0, (amount1 * totalSupply) / _reserve1);
         }
 
@@ -53,11 +68,66 @@ contract UniswapV2Pair is ERC20, Math {
         emit Mint(msg.sender, amount0, amount1);
     }
 
+    /**
+     *  @notice Burn liquidity tokens to withdraw the underlying tokens
+     *  @dev The amount of underlying tokens that can be withdrawn is proportional to the amount of liquidity tokens burned
+     *  UniswapV2 doesn't support partial burning,
+     *  so the amount of liquidity tokens burned must be equal to the total amount of liquidity tokens held by the user
+     */
+    function burn() public {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 liquidity = balanceOf[msg.sender];
+
+        uint256 amount0 = (liquidity * balance0) / totalSupply;
+        uint256 amount1 = (liquidity * balance1) / totalSupply;
+
+        if (amount0 <= 0 || amount1 <= 0) revert InsufficientLiquidityBurned();
+
+        _burn(msg.sender, liquidity);
+
+        _safeTransfer(token0, msg.sender, amount0);
+        _safeTransfer(token1, msg.sender, amount1);
+
+        balance0 = IERC20(token0).balanceOf(address(this));
+        balance1 = IERC20(token1).balanceOf(address(this));
+
+        _update(balance0, balance1);
+
+        emit Burn(msg.sender, amount0, amount1);
+    }
+
+    /**
+     * @notice Update the reserves of token0 and token1
+     * @param balance0  The amount of token0 that is in the contract
+     * @param balance1  The amount of token1 that is in the contract
+     */
     function _update(uint256 balance0, uint256 balance1) private {
         reserve0 = uint112(balance0);
         reserve1 = uint112(balance1);
+
+        emit Sync(reserve0, reserve1);
     }
 
+    /**
+     * @notice Transfer tokens safely
+     * @param token The address of the token to transfer
+     * @param to    The address to transfer the tokens to
+     * @param value The amount of tokens to transfer
+     */
+    function _safeTransfer(address token, address to, uint256 value) private {
+        (bool success, bytes memory data) = token.call(abi.encodeWithSignature("transfer(address,uint256)", to, value));
+        if (!success || (data.length != 0 && !abi.decode(data, (bool)))) {
+            revert TransferFailed();
+        }
+    }
+
+    /**
+     * @notice Get the reserves of token0 and token1
+     * @return reserve0 The amount of token0 that is in the contract
+     * @return reserve1 The amount of token1 that is in the contract
+     * @return 0        The current block timestamp
+     */
     function getReserves() public view returns (uint112, uint112, uint32) {
         return (reserve0, reserve1, 0);
     }
--- test/UniswapV2PairTest.t.sol
+++ test/UniswapV2PairTest.t.sol
@@ -4,12 +4,14 @@ pragma solidity 0.8.21;
 import {Test} from "forge-std/Test.sol";
 import {UniswapV2Pair} from "../src/UniswapV2Pair.sol";
 import {ERC20Mintable} from "./mocks/ERC20Mintable.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract UniswapV2PairTest is Test {
     ERC20Mintable public token0;
     ERC20Mintable public token1;
     UniswapV2Pair public pair;
 
+    TestUser public testUser = new TestUser();
     uint256 constant MINIMUM_LIQUIDITY = 1000; // 1e15
 
     function setUp() public {
@@ -65,9 +67,87 @@ contract UniswapV2PairTest is Test {
         assertReserves(3 ether, 2 ether);
     }
 
+    function testBurn() public {
+        // Provide initial liquidity
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint();
+        pair.burn();
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertReserves(1000, 1000);
+        assertEq(pair.totalSupply(), 1000);
+        assertEq(token0.balanceOf(address(this)), 10 ether - MINIMUM_LIQUIDITY);
+        assertEq(token1.balanceOf(address(this)), 10 ether - MINIMUM_LIQUIDITY);
+    }
+
+    function testBurnUnbalanced() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint();
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // + 1 LP
+
+        pair.burn();
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertReserves(1500, 1000);
+        assertEq(pair.totalSupply(), 1000);
+        // Here additional 500 wei is lost from token0.
+        // This is the punishment price for price manipulaiton.
+        assertEq(token0.balanceOf(address(this)), 10 ether - 1500);
+        assertEq(token1.balanceOf(address(this)), 10 ether - MINIMUM_LIQUIDITY);
+    }
+
+    function testBurnUnbalancedDifferentUsers() public {
+        testUser.provideLiquidity(address(pair), address(token0), address(token1), 1 ether, 1 ether);
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertEq(pair.balanceOf(address(testUser)), 1 ether - 1000);
+        assertEq(pair.totalSupply(), 1 ether);
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // + 1 LP
+
+        pair.burn();
+
+        // this user is penalized for providing unbalanced liquidity
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertReserves(1.5 ether, 1 ether);
+        assertEq(pair.totalSupply(), 1 ether);
+        assertEq(token0.balanceOf(address(this)), 10 ether - 0.5 ether);
+        assertEq(token1.balanceOf(address(this)), 10 ether);
+    }
+
     function assertReserves(uint112 expectedReserve0, uint112 expectedReserve1) internal {
         (uint112 reserve0, uint112 reserve1,) = pair.getReserves();
         assertEq(reserve0, expectedReserve0, "unexpected reserve0");
         assertEq(reserve1, expectedReserve1, "unexpected reserve1");
+    }
+}
+
+contract TestUser {
+    function provideLiquidity(
+        address pairAddress_,
+        address token0Address_,
+        address token1Address_,
+        uint256 amount0_,
+        uint256 amount1_
+    ) public {
+        ERC20(token0Address_).transfer(pairAddress_, amount0_);
+        ERC20(token1Address_).transfer(pairAddress_, amount1_);
+
+        UniswapV2Pair(pairAddress_).mint();
+    }
+
+    function withdrawLiquidity(address pairAddress_) public {
+        UniswapV2Pair(pairAddress_).burn();
     }
 }
